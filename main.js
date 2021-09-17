03e9fbc75872805e18bf7f44bc389aecbedd36f5
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-09-17 13:59:34 -0300

    Add event listener to buttons

--- main.js
+++ main.js
@@ -2,25 +2,22 @@ let dataPlans = [];
 let dataValues = [];
 let phonePlans = [];
 let phoneValues = [];
+
 async function getCombo(){
     try {
         const response = await fetch("http://localhost:3000/combo");
         const data = await response.json();
         showDataPlans(data);
         addDivFromArray(dataPlans, dataValues, "dataPlans");
-        // appendText("dataPlans", dataPlans);
-        // appendText("dataValues", dataValues);
         addDivFromArray(phonePlans, phoneValues, "phonePlans");
-        // appendText("phonePlans", phonePlans);
-        // appendText("phoneValues", phoneValues);
+        updateCart()
         
+
     
     } catch (error) {
     console.error(error);
     }
 }
-
-
 
 function showDataPlans(data) {
     for(let key in data.data){
@@ -50,24 +47,42 @@ function showDataPlans(data) {
         }
     }
 }
-function appendText(id, text){
-    let div = document.getElementById(id);
-    div.innerHTML = text;
-}
 
-function addDivFromArray(arr1, arr2, id){
-    for(let i=0; i<arr1.length; i++){
+function addDivFromArray(planArr, valueArr, id){
+    for(let i=0; i<planArr.length; i++){
         let newDiv = document.createElement("div");
         let newPlan = document.createElement("p");
-        let textNode = document.createTextNode(arr1[i]);
+        let textNode = document.createTextNode(planArr[i]);
         newPlan.appendChild(textNode);
+        newPlan.setAttribute("class", "plan");
         newDiv.appendChild(newPlan);
         let newValue = document.createElement("p");
-        textNode = document.createTextNode(arr2[i]);
+        textNode = document.createTextNode(valueArr[i]);
         newValue.appendChild(textNode);
+        newValue.setAttribute("class", "value");
         newDiv.appendChild(newValue);
+        let button = document.createElement("button");
+        button.innerHTML = "Selecionar"
+        button.setAttribute("class", "btn")
+        newDiv.appendChild(button);
+
         document.getElementById(id).appendChild(newDiv);
     }
 }
+
+
+function updateCart(){
+    let dataCart = document.getElementById('dataSelected')
+
+
+    let btns = document.querySelectorAll('.btn');
+    btns.forEach(btn => btn.addEventListener('click', (ev) => {
+        console.log('click');
+    }));
+}
+
+
+
+
 
 getCombo();
